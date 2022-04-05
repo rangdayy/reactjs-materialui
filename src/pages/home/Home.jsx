f196e6b418c6669b319b94929fea5cf1849f7b3f
unknown <choazranda@gmail.com>
2022-04-05 09:15:09 +0800

    dashboard structure

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import "./Home.scss"
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Sidebar />
+            <div className='homeContainer'>homeContainer</div>
+        </div>
+    )
+}
+
+export default Home
