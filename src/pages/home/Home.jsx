eead6af9efbee5c6f10f429daa6377c9eff7b1e5
unknown <choazranda@gmail.com>
2022-04-05 09:55:22 +0800

    simple dashboard finishes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,9 @@
+import { Table } from '@mui/material'
 import React from 'react'
+import Chart from '../../components/chart/Chart'
+import Featured from '../../components/featured/Featured'
+import Widget from '../../components/widget/Widget'
+import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import "./Home.scss"
 
@@ -6,7 +11,24 @@ const Home = () => {
     return (
         <div className='home'>
             <Sidebar />
-            <div className='homeContainer'>homeContainer</div>
+            <div className='homeContainer'>
+                <Navbar />
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
+                <div className="charts">
+                    <Featured />
+                    <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+                </div>
+                <div className="listContainer">
+                    <div className="listTitle">Latest Transactions</div>
+                    <Table />
+                </div>
+            </div>
+
         </div>
     )
 }
